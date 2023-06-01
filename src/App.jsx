d4c63a5f563c0b80265e6b9576d53f3eeae335fd
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-06-01 11:59:15 +0300

    Finished home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
-import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hero from "./pages/Hero";
+import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Hero />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
       <Footer />
     </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,83 @@
 import React from "react";
-import Hero from "../components/HomeComponents/Hero";
-import NavBar from "../components/NavBar";
-
+import homeabout from "../components/images/homeabout.png";
+import homeat45 from "../components/images/homeat45.png";
+import homeclubnews from "../components/images/homeclubnews.png";
+import homekababeri from "../components/images/homekababeri.png";
+import homeshop from "../components/images/homeshop.png";
+import { IoIosArrowForward } from "react-icons/io";
 const Home = () => {
   return (
-    <div>
-      <NavBar />
-      <Hero />
+    <div class="h-[100vh] flex justify-center">
+      <div className="w-2/5 ">
+        <div
+          className="h-[80vh] bg-center bg-cover bg-no-repeat"
+          style={{ backgroundImage: `url(${homeabout})` }}
+        >
+          <div className="h-[80vh] flex items-end justify-end pb-12 ">
+            <h1 className="bg-[#1F2024] bg-opacity-75 rounded-l-2xl  w-[210px] h-[80px] flex items-center justify-center text-white text-xl">
+              About us
+            </h1>
+          </div>
+        </div>
+        <div className="h-[20vh] flex items-center justify-center  bg-[#1F2024] ">
+          <h1 className="text-white text-7xl flex font-bold">
+            Join The Kulabu{" "}
+            <span>
+              <IoIosArrowForward className="text-7xl" />
+            </span>
+          </h1>
+        </div>
+      </div>
+      <div className="w-3/5   flex flex-col ">
+        <div className=" flex w-[100%] h-[50vh]">
+          <div
+            className="w-[60%] bg-center bg-cover bg-no-repeat"
+            style={{ backgroundImage: `url(${homeat45})` }}
+          >
+            <div className="h-[50vh] flex items-end justify-end pb-12 ">
+              <h1 className="bg-[#1F2024] bg-opacity-75 rounded-l-2xl  w-[210px] h-[80px] flex items-center justify-center text-white text-xl">
+                Mwamba at 45
+              </h1>
+            </div>
+          </div>
+          <div
+            className="w-[40%] bg-center bg-cover bg-no-repeat"
+            style={{
+              backgroundImage: `url(${homekababeri})`,
+            }}
+          >
+            <div className="h-[50vh] flex items-end justify-end pb-12 ">
+              <h1 className="bg-[#1F2024] bg-opacity-75 rounded-l-2xl  w-[210px] h-[80px] flex items-center justify-center text-white text-xl">
+                Kababeri Sevens
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="bg-black flex w-[100%] h-[50vh]">
+          <div
+            className="w-[40%] bg-center bg-cover bg-no-repeat"
+            style={{
+              backgroundImage: `url(${homeclubnews})`,
+            }}
+          >
+            <div className="h-[50vh] flex items-end justify-end pb-12 ">
+              <h1 className="bg-[#1F2024] bg-opacity-75 rounded-l-2xl  w-[210px] h-[80px] flex items-center justify-center text-white text-xl">
+                Club News
+              </h1>
+            </div>
+          </div>
+          <div
+            className="w-[60%] bg-center bg-cover bg-no-repeat"
+            style={{ backgroundImage: `url(${homeshop})` }}
+          >
+            <div className="h-[50vh] flex items-end justify-end pb-12 ">
+              <h1 className="bg-[#1F2024] bg-opacity-75 rounded-l-2xl  w-[315px] h-[80px] flex items-center justify-center text-white text-xl">
+                Buy Mwamba Merchandise
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
