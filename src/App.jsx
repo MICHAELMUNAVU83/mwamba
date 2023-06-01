f8b1e081088a895439cd92cb3ef53527b33e8eb4
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-06-01 08:51:35 +0300

    Added

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const [showOrderModal, setShowOrderModal] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowOrderModal(true);
+    }, 2000);
+  }, []);
+  return (
+    <div>
+      {showOrderModal && (
+        <div className="fixed kulim-park bg-white shadow-xl h-[500px] my-auto  inset-0 bg-opacity z-10 flex flex-col items-center justify-start w-[400px]  mx-auto">
+          <div className="flex justify-end w-full p-4">
+            <button
+              className="bg-black text-white px-4 py-2 rounded-lg"
+              onClick={() => setShowOrderModal(false)}
+            >
+              X
+            </button>
+          </div>
+          <form className="space-y-4 md:space-y-6">
+            <p>
+              <span className="text-2xl font-bold">Ticket data</span>
+            </p>
+            Modal for ticket sales...
+          </form>
+        </div>
+      )}
+
+      <p>Mwamba rugby club</p>
+    </div>
+  );
+}
+
+export default App;
