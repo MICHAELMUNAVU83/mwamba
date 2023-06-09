79702e7ecad3833cb7ff683f1500089f70e53989
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-06-09 11:16:51 +0300

    Added sliders

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -3,6 +3,11 @@ import "swiper/css";
 import "swiper/css/autoplay";
 import { Swiper, SwiperSlide } from "swiper/react";
 import slide1 from "../components/images/hero/slide1.png";
+import slide2 from "../components/images/hero/slide2.png";
+import slide3 from "../components/images/hero/slide3.png";
+import slide4 from "../components/images/hero/slide4.png";
+import slide5 from "../components/images/hero/slide5.png";
+import slide6 from "../components/images/hero/slide6.png";
 import { Pagination, Autoplay } from "swiper";
 
 const Hero = () => {
@@ -10,10 +15,28 @@ const Hero = () => {
     {
       id: 1,
       image: slide1,
+      text1: "Mwamba Rugby",
+      text2: "Football Club",
     },
     {
       id: 2,
-      image: slide1,
+      image: slide2,
+    },
+    {
+      id: 3,
+      image: slide3,
+    },
+    {
+      id: 4,
+      image: slide4,
+    },
+    {
+      id: 5,
+      image: slide5,
+    },
+    {
+      id: 6,
+      image: slide6,
     },
   ];
   return (
@@ -34,11 +57,11 @@ const Hero = () => {
         <SwiperSlide
           key={slide.id}
           style={{ backgroundImage: `url(${slide.image})` }}
-          className="bg-cover bg-no-repeat h-[100vh]  bg-center w-full "
+          className="bg-cover cursor-pointer bg-no-repeat h-[100vh]  bg-center w-full "
         >
           <div className="flex flex-col items-start p-12 text-white justify-center h-[100vh]">
-            <h1 className="text-[120px] font-bold">Mwamba Rugby</h1>
-            <h1 className="text-7xl font-bold">Football Club</h1>
+            <h1 className="text-[120px] font-bold">{slide.text1} </h1>
+            <h1 className="text-7xl font-bold">{slide.text2}</h1>
           </div>
         </SwiperSlide>
       ))}
